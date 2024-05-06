baba97ab47627d556a870e5bd1249b989990e234
Muskan Gupta <musgupta@adobe.com>
2024-05-06 14:39:19 +0530

    changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,9 +1,17 @@
-export const USER_AVATAR = "https://assets.leetcode.com/users/avatars/avatar_1650197078.png";
+export const USER_AVATAR =
+  "https://assets.leetcode.com/users/avatars/avatar_1650197078.png";
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzVhYzZlNDk1MjQwMGZkNGNhZjI1NDQwNmUyMzcyYyIsInN1YiI6IjY2MzY0MjdiOTU5MGUzMDEyM2JjMmY5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R3G46NKL-r_Xr0eMlW95aMhUxCSsiMt27GG8YpOdOoI'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzVhYzZlNDk1MjQwMGZkNGNhZjI1NDQwNmUyMzcyYyIsInN1YiI6IjY2MzY0MjdiOTU5MGUzMDEyM2JjMmY5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R3G46NKL-r_Xr0eMlW95aMhUxCSsiMt27GG8YpOdOoI",
+  },
 };
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
